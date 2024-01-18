ff5916b4efe325a974a4bee46da1509b1e402cd3
Ilona van Oosbree <144005439+smolgeorgie@users.noreply.github.com>
2024-01-18 18:58:45 +0100

    more js

--- script/main.js
+++ script/main.js
@@ -7,7 +7,7 @@ for (var i = 0; i < readmore.length; i++) {
     let readmoreContainer = el.closest("article");
     readmoreContainer.classList.toggle("open");
     
-    let lable = (el.innerHTML === "More") ? "Less" : "More";
+    let lable = (el.innerHTML === "+ Lees de volledige beschrijving") ? "Lees minder beschrijving" : " + Lees meer beschrijving";
     el.innerHTML = lable;
     
     return false;

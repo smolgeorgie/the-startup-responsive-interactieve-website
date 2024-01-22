45a7bf587586824fbbfcff71cd30e2ea87cdbbf1
Ilona van Oosbree <144005439+smolgeorgie@users.noreply.github.com>
2024-01-22 13:25:12 +0100

    ITS WORKING - js

--- script/main.js
+++ script/main.js
@@ -1,17 +1,29 @@
-var readmore = document.querySelectorAll(".readmore-link a");
+window.onload = function() {
+  var articles = document.querySelectorAll('.readmore');
+  var buttons = document.querySelectorAll('.toggle');
 
-for (var i = 0; i < readmore.length; i++) {
-  readmore[i].onclick = function() {
-    
-    let readmoreContainer = this.closest("article");
-    readmoreContainer.classList.toggle("open");
-    
-    if (readmoreContainer.classList.contains("open")) {
-      this.innerHTML = "+ Voor meer informatie";
-    } else {
-      this.innerHTML = "- Voor minder informatie";
+  for (var i = 0; i < articles.length; i++) {
+    // Initially hide all but the first paragraph
+    var paragraphs = articles[i].querySelectorAll('p');
+    for (var j = 1; j < paragraphs.length; j++) {
+      paragraphs[j].style.display = 'none';
     }
-    
-    return false;
-  };
+
+    buttons[i].addEventListener('click', function() {
+      var article = this.closest('.readmore'); // Use closest to find the parent article
+      var paragraphs = article.querySelectorAll('p');
+      
+      if (this.textContent === 'Read more') {
+        for (var j = 1; j < paragraphs.length; j++) {
+          paragraphs[j].style.display = 'block';
+        }
+        this.textContent = 'Read less';
+      } else {
+        for (var j = 1; j < paragraphs.length; j++) {
+          paragraphs[j].style.display = 'none';
+        }
+        this.textContent = 'Read more';
+      }
+    });
+  }
 }
